5771be5df1827587263cf63caf1f5094ab971f51
Kwonyoungoh <rnjsduddh05@gmail.com>
2024-04-23 17:09:27 +0900

    worksflow test2

--- app/index.js
+++ app/index.js
@@ -10,7 +10,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={
             <>
-              <div>Hello!! This is S3 hosting. v1</div>
+              <div>Hello!! This is S3 hosting. test</div>
             </>
           } />
         </Routes>
