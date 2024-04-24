4d805eb3abfb6b37bf8741bec4816f92dfc8facd
Kwonyoungoh <rnjsduddh05@gmail.com>
2024-04-24 17:55:15 +0900

    cdn cache test

---
+++ README.md
@@ -0,0 +1,4 @@
+# 0424 아키텍처 스터디
+## 프론트엔드 Repo
+### Architecture
+![s3 cicd](./Diagram/study0424.drawio.png)
--- app/index.js
+++ app/index.js
@@ -11,7 +11,7 @@ const App = () => {
           <Route path="/" element={
             <>
               <div>Hello!! This is S3 hosting. hosting is good</div>
-              <div> cache test </div>
+              <div> cdn cache test </div>
             </>
           } />
         </Routes>
